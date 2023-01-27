3e3078bae50ac6713ceda986cfc7ccc53b5ca5b5
Mohamed Shehata <100875413+MoShehata7920@users.noreply.github.com>
2023-01-27 18:04:26 +0200

    initialized the mobile app

---
+++ mobile/android/app/src/main/kotlin/com/example/sed/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.sed
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ mobile/lib/app/app.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
