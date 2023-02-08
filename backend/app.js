5593d123d2437bf253e498ea92d8b4db3dd80c10
Mahmoud Elamrosy <74033364+melamrosy72@users.noreply.github.com>
2023-02-08 03:14:38 +0200

    Testing Server Connection Done

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,13 @@
 const express=require('express')
 require('dotenv').config()
-
+const mongoose=require('mongoose')
 const PORT=process.env.PORT || 8080
 
 const app=express()
 
-
-
+app.get('',(req,res)=>{
+    res.send('Hello Eagles <3 ')
+})
 
 app.listen(PORT,()=>{
     console.log(`The Server Is Working And listening to Port ${PORT}`);
