d02b4b0d845b3cd56bc4ee387682413081bc0ba1
Mahmoud Elamrosy <74033364+melamrosy72@users.noreply.github.com>
2023-02-08 03:53:06 +0200

    connecting to DB

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,11 @@ const PORT=process.env.PORT || 8080
 
 const app=express()
 
+//mongoose Connection
+mongoose.set('strictQuery', true)        //  suppressing the warning of the new coming update in mongoose 7 
+mongoose.connect(process.env.MONGO_URL).then(()=>{console.log('Connected Successfully To SED Database')})
+
+
 app.get('',(req,res)=>{
     res.send('Hello Eagles <3 ')
 })
