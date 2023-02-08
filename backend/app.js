14f63558eaf96b0e6d7530e97761437b29f1235a
Mahmoud Elamrosy <74033364+melamrosy72@users.noreply.github.com>
2023-02-08 04:09:17 +0200

    added general routing error handler and packages

--- backend/app.js
+++ backend/app.js
@@ -2,8 +2,11 @@ const express=require('express')
 require('dotenv').config()
 const mongoose=require('mongoose')
 const PORT=process.env.PORT || 8080
-
+const morgan = require('morgan')
 const app=express()
+const cors = require('cors')
+app.use(morgan('dev'))
+app.use(cors())
 
 //mongoose Connection
 mongoose.set('strictQuery', true)        //  suppressing the warning of the new coming update in mongoose 7 
@@ -13,6 +16,20 @@ mongoose.connect(process.env.MONGO_URL).then(()=>{console.log('Connected Success
 app.get('',(req,res)=>{
     res.send('Hello Eagles <3 ')
 })
+
+
+//handling general error in the request       // must be after all routings
+app.use((req,res,next)=>{
+    const error=new Error('Not Found')
+    error.status=404
+    next(error)
+})
+
+app.use((error,req,res)=>{
+    console.log(error.message);
+    res.status(error.status || 500).json({error : 'Not Found'})
+})
+
 
 app.listen(PORT,()=>{
     console.log(`The Server Is Working And listening to Port ${PORT}`);
