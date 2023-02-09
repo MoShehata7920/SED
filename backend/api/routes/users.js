7d523e5b4a6588fff2a2c2e7620f0136c37b9a2c
Mahmoud Elamrosy <74033364+melamrosy72@users.noreply.github.com>
2023-02-09 23:53:29 +0200

    created login route in users dot js

--- backend/api/routes/users.js
+++ backend/api/routes/users.js
@@ -28,11 +28,27 @@ router.post('/register',(req,res)=>{
             }else{
                 res.status(406).json('Please make sure your passwords match')
             }
-            
-
         }
     })
+})
 
+router.post('/login',(req,res)=>{
+    User.find({email:req.body.email}).exec().then((user)=>{
+        if(user.length>=1){                                                                                     // email found
+            bcrypt.compare(req.body.password,user[0].password,(err,result)=>{
+                if(err){                                                                                        // server error
+                    res.status(500).json(err)
+                }else if(result) {                                                                              // true password
+                    res.status(200).json(`welcome back ${user[0].fullName}!`)
+                }
+                else{                                                                                           // wrong password
+                    res.status(401).json('Wrong password. Try again or click Forgot password to reset it.')
+                }
+            })
+        }else{                                                                                                  //email not found 
+            return res.status(406).json('There is no account registered with this email')
+        }
+    })
 })
 
 
