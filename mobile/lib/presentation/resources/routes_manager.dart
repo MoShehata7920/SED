a25ac357fa02720de6e83a80a70c393585815225
Mohamed Shehata <100875413+MoShehata7920@users.noreply.github.com>
2023-02-10 02:30:57 +0200

    finished splash screen

--- mobile/lib/presentation/resources/routes_manager.dart
+++ mobile/lib/presentation/resources/routes_manager.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:sed/presentation/resources/strings_manager.dart';
 
@@ -9,7 +8,6 @@ class Routes {
 }
 
 class RouteGenerator {
-
   static Route<dynamic> getRoute(RouteSettings settings) {
     switch (settings.name) {
       case Routes.splashRoute:
@@ -20,10 +18,12 @@ class RouteGenerator {
   }
 
   static Route<dynamic> unDefinedRoute() {
-    return MaterialPageRoute(builder: (context) => Scaffold(
-      appBar: AppBar(title: const Text(AppStrings.noRouteTitle),),
-      body: const Center(child: Text(AppStrings.noRouteFound)),
-    ));
+    return MaterialPageRoute(
+        builder: (context) => Scaffold(
+              appBar: AppBar(
+                title: const Text(AppStrings.noRouteTitle),
+              ),
+              body: const Center(child: Text(AppStrings.noRouteFound)),
+            ));
   }
-
 }
