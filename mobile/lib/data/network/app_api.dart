c4a0ff3c13ce8948bfe8d37a8e24f94cc29d75ab
Mohamed Shehata <100875413+MoShehata7920@users.noreply.github.com>
2023-02-12 21:01:06 +0200

    added API's responses and service client

---
+++ mobile/lib/data/network/app_api.dart
@@ -0,0 +1,17 @@
+import 'package:dio/dio.dart';
+import 'package:retrofit/http.dart';
+import 'package:sed/app/constants.dart';
+import 'package:sed/data/responses/responses.dart';
+
+part 'app_api.g.dart';
+
+@RestApi(baseUrl: Constants.baseUrl)
+abstract class AppServiceClient {
+  factory AppServiceClient(Dio dio, {String baseUrl}) = _AppServiceClient;
+
+  @POST("/customers/login")
+  Future<AuthenticationResponse> login(
+    @Field("email") String email ,
+    @Field("password") String password ,
+  );
+}
---
+++ mobile/lib/data/responses/responses.g.dart
@@ -0,0 +1,33 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'responses.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+BaseResponse _$BaseResponseFromJson(Map<String, dynamic> json) => BaseResponse()
+  ..status = json['status'] as int?
+  ..message = json['message'] as String?;
+
+Map<String, dynamic> _$BaseResponseToJson(BaseResponse instance) =>
+    <String, dynamic>{
+      'status': instance.status,
+      'message': instance.message,
+    };
+
+AuthenticationResponse _$AuthenticationResponseFromJson(
+        Map<String, dynamic> json) =>
+    AuthenticationResponse(
+      json['token'] as String?,
+    )
+      ..status = json['status'] as int?
+      ..message = json['message'] as String?;
+
+Map<String, dynamic> _$AuthenticationResponseToJson(
+        AuthenticationResponse instance) =>
+    <String, dynamic>{
+      'status': instance.status,
+      'message': instance.message,
+      'token': instance.token,
+    };
