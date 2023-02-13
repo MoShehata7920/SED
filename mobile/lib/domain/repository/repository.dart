25768b071b0668cbb4bd7f0d90b1f1f328d8e6bd
Bimbum1337 <mahmoudhafez1400@gmail.com>
2023-02-13 08:17:41 +0200

    finished the mapping

---
+++ mobile/lib/domain/repository/repository.dart
@@ -0,0 +1,8 @@
+import 'package:dartz/dartz.dart';
+import 'package:sed/data/network/failure.dart';
+import 'package:sed/data/network/requests.dart';
+import 'package:sed/domain/model/models.dart';
+
+abstract class Repository {
+  Future<Either<Failure,Authentication>> login(LoginRequest loginRequest);
+}
