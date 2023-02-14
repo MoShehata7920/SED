a97999ee6dac0aadb743776fc0b5245cad12648c
Mahmoud Elamrosy <74033364+melamrosy72@users.noreply.github.com>
2023-02-14 06:07:33 +0200

    created check-auth middleware to check token

---
+++ backend/middleware/check-auth.js
@@ -0,0 +1,21 @@
+const jwt=require('jsonwebtoken')
+
+const verifyToken=(req,res,next)=>{
+    try {
+        const token=req.headers.authentication.split('')
+        const checker=jwt.verify(token[1] , process.env.SECRET_KEY , (err,decoded)=>{
+            if(err){
+                res.status(403).json('Token Not Vaild')
+            }
+            // console.log("the decoded data : " + JSON.stringify(decoded)) 
+            req.user=decoded
+            next()    // to continue to the next function 
+
+        })
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({message:"Error Authentication Failed"})
+    }
+}
+
+module.exports=verifyToken;
