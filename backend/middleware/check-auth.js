9cef473d9609a35af582e59496d2830ac2aa4fcc
Mahmoud Elamrosy <74033364+melamrosy72@users.noreply.github.com>
2023-02-14 06:19:25 +0200

    completed verifyToken function

--- backend/middleware/check-auth.js
+++ backend/middleware/check-auth.js
@@ -2,7 +2,7 @@ const jwt=require('jsonwebtoken')
 
 const verifyToken=(req,res,next)=>{
     try {
-        const token=req.headers.authentication.split('')
+        const token=req.headers.authintication.split(' ')
         const checker=jwt.verify(token[1] , process.env.SECRET_KEY , (err,decoded)=>{
             if(err){
                 res.status(403).json('Token Not Vaild')
