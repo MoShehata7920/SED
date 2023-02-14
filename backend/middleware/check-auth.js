ff4ac138e4e7d0ea72d3d3acd90fcbb87594e2ac
Mahmoud Elamrosy <74033364+melamrosy72@users.noreply.github.com>
2023-02-14 06:38:52 +0200

    created admin checker fn

--- backend/middleware/check-auth.js
+++ backend/middleware/check-auth.js
@@ -2,7 +2,7 @@ const jwt=require('jsonwebtoken')
 
 const verifyToken=(req,res,next)=>{
     try {
-        const token=req.headers.authintication.split(' ')
+        const token=req.headers.authentication.split(' ')
         const checker=jwt.verify(token[1] , process.env.SECRET_KEY , (err,decoded)=>{
             if(err){
                 res.status(403).json('Token Not Vaild')
@@ -18,4 +18,15 @@ const verifyToken=(req,res,next)=>{
     }
 }
 
-module.exports=verifyToken;
+const verifyTokenAndAdmin=(req,res,next)=>{     // a function to check if the one who tries to access something is loggedin & admin
+    verifyToken(req,res,()=>{
+        if(req.user.isAdmin){
+            next()
+        }else{
+            res.status(403).json({message:'You Are Not Allowed to do that'})
+        }
+    })
+}
+
+
+module.exports={verifyToken,verifyTokenAndAdmin};
