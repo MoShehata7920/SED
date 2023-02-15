2061007a9995b2036b1e48d1a75b38a1dae9eaed
Mahmoud Elamrosy <74033364+melamrosy72@users.noreply.github.com>
2023-02-15 17:45:41 +0200

    created new product router

--- backend/api/routes/products.js
+++ backend/api/routes/products.js
@@ -3,11 +3,20 @@ const Product=require('../models/product')
 const mongoose=require('mongoose')
 
 
+router.post('/newproduct',(req,res)=>{
+    new Product({
+        productName : req.body.productName,
+        description : req.body.description,
+        quantity : req.body.quantity,
+        prdouctImage : req.file.path,
+        price : req.body.price,
 
-
-
-
-
+    }).save().then((newproduct)=>{
+        res.status(200).json(newproduct)
+    }).catch(err=>{
+        console.log(err);
+    })
+})
 
 
 
