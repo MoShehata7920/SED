d23e47e8c2ee8aa704ab62fde74d6afdd64bf19e
Mohamed Shehata <100875413+MoShehata7920@users.noreply.github.com>
2023-02-24 21:38:14 +0200

    added stream builder inside register view

--- mobile/lib/presentation/register/view/register_view.dart
+++ mobile/lib/presentation/register/view/register_view.dart
@@ -1,6 +1,9 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:sed/presentation/register/viewmodel/register_viewmodel.dart';
+import 'package:sed/presentation/resources/color_manager.dart';
+import 'package:sed/presentation/resources/values_manager.dart';
 import '../../../app/di.dart';
+import '../../common/state_renderer/state_renderer_impl.dart';
 
 class RegisterView extends StatefulWidget {
   const RegisterView({Key? key}) : super(key: key);
@@ -49,6 +52,24 @@ class _RegisterViewState extends State<RegisterView> {
 
   @override
   Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: ColorManager.white,
+      appBar: AppBar(
+        elevation: AppSize.s0,
+        backgroundColor: ColorManager.white,
+      ),
+      body: StreamBuilder<FlowState>(
+        stream: _viewModel.outputState,
+        builder: (context, snapshot) {
+          return snapshot.data?.getScreenWidget(
+                  context, _getContentWidget(), () => _viewModel.register()) ??
+              _getContentWidget();
+        },
+      ),
+    );
+  }
+
+  Widget _getContentWidget() {
     return Container();
   }
 
