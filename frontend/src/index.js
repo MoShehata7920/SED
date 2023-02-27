9f75a3d26d95aa88c053f8094c18bba1b49ee69f
abdelrhman ezzat saad <101066128+abdoezzat1323@users.noreply.github.com>
2023-02-27 20:55:45 +0200

    bootstrab i

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import MainRoutes from "./App";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import "./fontawesome-free-6.3.0-web/css/all.min.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
