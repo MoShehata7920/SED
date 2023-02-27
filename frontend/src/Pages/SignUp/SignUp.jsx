8d57ca71477d70c5db627563dd3def32500d6a45
abdelrhman ezzat saad <101066128+abdoezzat1323@users.noreply.github.com>
2023-02-27 22:39:58 +0200

    front start

--- frontend/src/Pages/SignUp/SignUp.jsx
+++ frontend/src/Pages/SignUp/SignUp.jsx
@@ -1,29 +1,3 @@
-import { useEffect } from "react";
-import { useState } from "react";
-import axios from "axios";
-
-function SignUp() {
-  const [count, setcount] = useState([]);
-  // const changeCount = () => {
-  //   setcount(Math.random());
-  // };
-  async function getdataa() {
-    let respond = await axios.get("");
-    console.log();
-    setcount();
-  }
-  useEffect(() => {
-    getdataa();
-  }, []);
-  return (
-    <>
-      <div>
-        {getdataa.map((movie, index) => (
-          <div key={index}></div>
-        ))}
-      </div>
-    </>
-  );
-}
+function SignUp() {}
 
 export default SignUp;
