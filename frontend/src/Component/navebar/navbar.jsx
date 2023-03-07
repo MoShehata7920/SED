c4dd196876240d5632b76b4cd3da0cca4710cc27
abdelrhman ezzat saad <101066128+abdoezzat1323@users.noreply.github.com>
2023-03-07 16:09:26 +0200

    home editing

--- frontend/src/Component/navebar/navbar.jsx
+++ frontend/src/Component/navebar/navbar.jsx
@@ -1,19 +1,19 @@
 import "./navbar.css";
 import { CiUser } from "react-icons/ci";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 function Navebar() {
   return (
     <>
-      <nav class="navbar navbar-expand-lg  pt-4 pb-4 bg-nav">
+      <nav className="navbar navbar-expand-lg  pt-4 pb-4 bg-nav">
         <div className="container">
           <div className="col-xl-2 col-lg-1 col-xxl-3 col-md-2 col-sm-10 col-10">
-            <Link class="navbar-brand text-white fs-3" to={"/"}>
+            <NavLink className="navbar-brand text-white fs-3" to={"/"}>
               SED
-            </Link>
+            </NavLink>
           </div>
           <div className="  col-">
             <button
-              class="navbar-toggler"
+              className="navbar-toggler"
               type="button"
               data-bs-toggle="collapse"
               data-bs-target="#navbarSupportedContent"
@@ -21,86 +21,86 @@ function Navebar() {
               aria-expanded="false"
               aria-label="Toggle navigation"
             >
-              <span class="navbar-toggler-icon"></span>
+              <span className="navbar-toggler-icon"></span>
             </button>
           </div>
           <div
-            class="collapse navbar-collapse col-5"
+            className="collapse navbar-collapse col-5"
             id="navbarSupportedContent"
           >
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0 ">
-              <li class="nav-item dropdown ">
-                <Link
-                  class="nav-link dropdown-toggle text-white fs-4 "
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0 ">
+              <li className="nav-item dropdown ">
+                <NavLink
+                  className="nav-NavLink dropdown-toggle text-white fs-4 "
                   to={"a"}
                   role="button"
                   data-bs-toggle="dropdown"
                   aria-expanded="false"
                 >
                   Categories
-                </Link>
-                <ul class="dropdown-menu ">
+                </NavLink>
+                <ul className="dropdown-menu ">
                   <li>
-                    <Link class="dropdown-item " to={"a"}>
+                    <NavLink className="dropdown-item " to={"a"}>
                       Action
-                    </Link>
+                    </NavLink>
                   </li>
                   <li>
-                    <Link class="dropdown-item " to={"a"}>
+                    <NavLink className="dropdown-item " to={"a"}>
                       Another action
-                    </Link>
+                    </NavLink>
                   </li>
                   <li>
-                    <Link class="dropdown-item " to={"a"}>
+                    <NavLink className="dropdown-item " to={"a"}>
                       Something else here
-                    </Link>
+                    </NavLink>
                   </li>
                 </ul>
               </li>
-              <li class="nav-item dropdown">
-                <Link
-                  class="nav-link dropdown-toggle text-white fs-4 ms-2 me-2"
+              <li className="nav-item dropdown">
+                <NavLink
+                  className="nav-NavLink dropdown-toggle text-white fs-4 ms-2 me-2"
                   to={"a"}
                   role="button"
                   data-bs-toggle="dropdown"
                   aria-expanded="false"
                 >
                   Services
-                </Link>
-                <ul class="dropdown-menu">
+                </NavLink>
+                <ul className="dropdown-menu">
                   <li>
-                    <Link class="dropdown-item text-black" to={"a"}>
+                    <NavLink className="dropdown-item text-black" to={"a"}>
                       SELL
-                    </Link>
+                    </NavLink>
                   </li>
                   <li>
-                    <Link class="dropdown-item text-black" to={"a"}>
+                    <NavLink className="dropdown-item text-black" to={"a"}>
                       EXCHANGE
-                    </Link>
+                    </NavLink>
                   </li>
                   <li>
-                    <Link class="dropdown-item text-black" to={"a"}>
+                    <NavLink className="dropdown-item text-black" to={"a"}>
                       DONAT
-                    </Link>
+                    </NavLink>
                   </li>
                 </ul>
               </li>
-              <li class="nav-item">
-                <Link class="nav-link text-white fs-4" to={"a"}>
+              <li className="nav-item">
+                <NavLink className="nav-NavLink text-white fs-4" to={"a"}>
                   Brand Stores
-                </Link>
+                </NavLink>
               </li>
             </ul>
-            <form class="d-flex" role="search">
-              <div class="input-group me-5">
+            <form className="d-flex" role="search">
+              <div className="input-group me-5">
                 <input
                   type="text"
                   aria-label="First name"
-                  class="form-control"
+                  className="form-control"
                   placeholder="Search . . ."
                 />
                 <button
-                  class="btn btn-outline-success text-white"
+                  className="btn btn-outline-success text-white"
                   type="submit"
                 >
                   Search
@@ -109,12 +109,12 @@ function Navebar() {
             </form>
             <ul className="navbar-nav ">
               <li className="nav-item">
-                <Link
+                <NavLink
                   to={"SignIn"}
-                  className="text-white nav-link fs-1 d-flex align-items-center justify-content-center "
+                  className="text-white nav-NavLink fs-1 d-flex align-items-center justify-content-center "
                 >
                   <CiUser />
-                </Link>
+                </NavLink>
               </li>
             </ul>
           </div>
--- frontend/src/Pages/Home/Home.jsx
+++ frontend/src/Pages/Home/Home.jsx
@@ -2,9 +2,10 @@ import Navebar from "../../Component/navebar/navbar";
 import "./Home.css";
 import "react-responsive-carousel/lib/styles/carousel.css";
 import OwlCarousel from "react-owl-carousel";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import Axios from "axios";
 import { useEffect, useState } from "react";
+import Footer from "../../Component/footer/Footer";
 
 export default function Home() {
   const options = {
@@ -65,8 +66,12 @@ export default function Home() {
     let { data } = await Axios.get(
       `http://103.48.193.225:9000/home/${mediatype}`
     );
-    console.log(data.items);
-    callback(data.items);
+
+    if (mediatype === "") {
+      callback(data.carousel.Images);
+    } else {
+      callback(data.items);
+    }
   }
 
   useEffect(() => {
@@ -82,73 +87,17 @@ export default function Home() {
       </section>
       <section className="pt-5">
         <OwlCarousel className="slider-style " {...options}>
-          <div className="item">
-            <img
-              className=""
-              src="https://thumbs.dreamstime.com/b/lake-clouds-sky-water-panoramic-panorama-banner-15611956.jpg"
-              alt=""
-            />
-          </div>
-          <div className="item">
-            <img
-              src="https://media.istockphoto.com/id/163196980/photo/sunset-panorama.jpg?s=612x612&w=0&k=20&c=kHv1TLoxBv5D2wZVnFUvyrU4KFbvJ9tEiXoG7h9y6ig="
-              alt=""
-            />
-          </div>
-          <div className="item">
-            <img
-              src="https://images.pexels.com/photos/358482/pexels-photo-358482.jpeg?cs=srgb&dl=pexels-pixabay-358482.jpg&fm=jpg"
-              alt=""
-            />
-          </div>
-          <div className="item">
-            <img
-              src="https://expertphotography.b-cdn.net/wp-content/uploads/2020/02/Photoshop.panorama.stitch.mishra.chicago.skyline-1024x290.jpg"
-              alt=""
-            />
-          </div>
-          <div className="item">
-            <img
-              src="https://www.kraftwerk.at/app/uploads/fly-images/962/reference-img-worlds-of-adventure-park-4-1920x9999.jpg"
-              alt=""
-            />
-          </div>
-          <div className="item">
-            <img
-              src="https://www.kraftwerk.at/app/uploads/fly-images/962/reference-img-worlds-of-adventure-park-4-1920x9999.jpg"
-              alt=""
-            />
-          </div>
-          <div className="item">
-            <img
-              src="https://www.kraftwerk.at/app/uploads/fly-images/962/reference-img-worlds-of-adventure-park-4-1920x9999.jpg"
-              alt=""
-            />
-          </div>
-          <div className="item">
-            <img
-              src="https://www.kraftwerk.at/app/uploads/fly-images/962/reference-img-worlds-of-adventure-park-4-1920x9999.jpg"
-              alt=""
-            />
-          </div>
-          <div className="item">
-            <img
-              src="https://www.kraftwerk.at/app/uploads/fly-images/962/reference-img-worlds-of-adventure-park-4-1920x9999.jpg"
-              alt=""
-            />
-          </div>
-          <div className="item">
-            <img
-              src="https://www.kraftwerk.at/app/uploads/fly-images/962/reference-img-worlds-of-adventure-park-4-1920x9999.jpg"
-              alt=""
-            />
-          </div>
+          {DetaAll.map((carous, carousindex) => (
+            <div key={carousindex} className="item">
+              <img className="" src={carous} alt="" />
+            </div>
+          ))}
         </OwlCarousel>
       </section>
       <section>
         <div className="row">
           <div className="col-xxl-3 col-xl-3 col-md-6 col-sm-12  col-12 ">
-            <Link to={"a"} className="  text-decoration-none">
+            <NavLink to={"aa"} className="  text-decoration-none">
               <div className="catogiry-div  position-relative  ">
                 <div className=" position-absolute h-100 w-100">
                   <img
@@ -161,10 +110,10 @@ export default function Home() {
                   <h4 className=" pb-3 ">Phone & accsessories</h4>
                 </div>
               </div>
-            </Link>
+            </NavLink>
           </div>
           <div className="col-xxl-3 col-xl-3 col-md-6 col-sm-12  col-12">
-            <Link to={"a"} className=" text-decoration-none">
+            <NavLink to={"a"} className=" text-decoration-none">
               <div className="catogiry-div  position-relative ">
                 <div className=" position-absolute w-100 h-100">
                   <img
@@ -177,10 +126,10 @@ export default function Home() {
                   <h4 className=" pb-3 ">Home Intertainment</h4>
                 </div>
               </div>
-            </Link>
+            </NavLink>
           </div>
           <div className="col-xxl-3 col-xl-3 col-md-6 col-sm-12  col-12 ">
-            <Link to={"a"} className="  text-decoration-none">
+            <NavLink to={"a"} className="  text-decoration-none">
               <div className="catogiry-div  position-relative   ">
                 <div className=" position-absolute w-100 h-100">
                   <img
@@ -193,10 +142,10 @@ export default function Home() {
                   <h4 className=" pb-3 ">Phone & accsessories</h4>
                 </div>
               </div>
-            </Link>
+            </NavLink>
           </div>
           <div className="col-xxl-3 col-xl-3 col-md-6 col-sm-12  col-12 ">
-            <Link to={"a"} className="  text-decoration-none  ">
+            <NavLink to={"a"} className="  text-decoration-none  ">
               <div className="catogiry-div  position-relative d-flex justify-content-center">
                 <div className=" position-absolute w-100 h-100">
                   <img
@@ -209,7 +158,7 @@ export default function Home() {
                   <h4 className=" pb-3 ms-3 ">Computer & Tablet</h4>
                 </div>
               </div>
-            </Link>
+            </NavLink>
           </div>
         </div>
       </section>
@@ -229,7 +178,7 @@ export default function Home() {
       <section>
         <div className="row ">
           <div className="col-xxl-3 col-xl-3 col-md-6 col-sm-12  col-12 ">
-            <Link to={"a"} className="  text-decoration-none">
+            <NavLink to={"a"} className="  text-decoration-none">
               <div className="catogiry-div  position-relative  ">
                 <div className=" position-absolute h-100 w-100">
                   <img
@@ -242,10 +191,10 @@ export default function Home() {
                   <h4 className=" pb-3 ">Lage Home Appliances</h4>
                 </div>
               </div>
-            </Link>
+            </NavLink>
           </div>
           <div className="col-xxl-3 col-xl-3 col-md-6 col-sm-12  col-12">
-            <Link to={"a"} className=" text-decoration-none">
+            <NavLink to={"a"} className=" text-decoration-none">
               <div className="catogiry-div  position-relative ">
                 <div className=" position-absolute w-100 h-100">
                   <img
@@ -258,10 +207,10 @@ export default function Home() {
                   <h4 className=" pb-3 ">Personla Care</h4>
                 </div>
               </div>
-            </Link>
+            </NavLink>
           </div>
           <div className="col-xxl-3 col-xl-3 col-md-6 col-sm-12  col-12 ">
-            <Link to={"a"} className="  text-decoration-none">
+            <NavLink to={"a"} className="  text-decoration-none">
               <div className="catogiry-div  position-relative   ">
                 <div className=" position-absolute w-100 h-100">
                   <img
@@ -274,10 +223,10 @@ export default function Home() {
                   <h4 className=" pb-3 ">Gaming</h4>
                 </div>
               </div>
-            </Link>
+            </NavLink>
           </div>
           <div className="col-xxl-3 col-xl-3 col-md-6 col-sm-12  col-12 ">
-            <Link to={"a"} className="  text-decoration-none  ">
+            <NavLink to={"a"} className="  text-decoration-none  ">
               <div className="catogiry-div  position-relative d-flex justify-content-center">
                 <div className=" position-absolute w-100 h-100">
                   <img
@@ -290,7 +239,7 @@ export default function Home() {
                   <h4 className=" pb-3 ms-3 ">Computer & Tablet</h4>
                 </div>
               </div>
-            </Link>
+            </NavLink>
           </div>
         </div>
       </section>
@@ -298,13 +247,13 @@ export default function Home() {
         <OwlCarousel className=" owl-theme  " {...options2}>
           <div className="item text-center">
             <h1 className=" fs-10 ">SELL</h1>
-            <Link to={"a"}>
+            <NavLink to={"a"}>
               <p className="text-black fs-3 "> see all</p>
-            </Link>
+            </NavLink>
           </div>
           {Detasell.map((sell, index) => (
-            <Link
-              key={sell.id}
+            <NavLink
+              key={index}
               to={`/items/${sell.ID}`}
               className="text-decoration-none "
             >
@@ -332,17 +281,17 @@ export default function Home() {
 
                   <div className="slider-service-btn">
                     <button className="btn btn-primary mt-2 mb-3">
-                      <Link
+                      <NavLink
                         to={"n"}
                         className="text-decoration-none text-white"
                       >
                         ADD TO CARD
-                      </Link>
+                      </NavLink>
                     </button>
                   </div>
                 </div>
               </div>
-            </Link>
+            </NavLink>
           ))}
         </OwlCarousel>
       </section>
@@ -350,12 +299,12 @@ export default function Home() {
         <OwlCarousel className=" owl-theme  " {...options2}>
           <div className="item text-center">
             <h1 className=" fs-9 ">EXCHANGE</h1>
-            <Link to={"a"}>
+            <NavLink to={"a"}>
               <p className="text-black fs-3 "> see all</p>
-            </Link>
+            </NavLink>
           </div>
           {Detaexchange.map((exchange, ind) => (
-            <Link
+            <NavLink
               key={ind}
               to={`/items/${exchange.ID}`}
               className="text-decoration-none "
@@ -389,17 +338,17 @@ export default function Home() {
 
                   <div className="slider-service-btn">
                     <button className="btn btn-primary mt-2 mb-3">
-                      <Link
+                      <NavLink
                         to={"n"}
                         className="text-decoration-none text-white"
                       >
                         ADD TO CARD
-                      </Link>
+                      </NavLink>
                     </button>
                   </div>
                 </div>
               </div>
-            </Link>
+            </NavLink>
           ))}
         </OwlCarousel>
       </section>
@@ -407,13 +356,13 @@ export default function Home() {
         <OwlCarousel className=" owl-theme  " {...options2}>
           <div className="item text-center">
             <h1 className=" fs-9 ">DONATE</h1>
-            <Link to={"a"}>
+            <NavLink to={"a"}>
               <p className="text-black fs-3 "> see all</p>
-            </Link>
+            </NavLink>
           </div>
-          {Detadonat.map((donate, ind) => (
-            <Link
-              key={ind}
+          {Detadonat.map((donate, donateindex) => (
+            <NavLink
+              key={donateindex}
               to={`/items/${donate.ID}`}
               className="text-decoration-none "
             >
@@ -446,127 +395,21 @@ export default function Home() {
 
                   <div className="slider-service-btn">
                     <button className="btn btn-primary mt-2 mb-3">
-                      <Link
+                      <NavLink
                         to={"n"}
                         className="text-decoration-none text-white"
                       >
                         ADD TO CARD
-                      </Link>
+                      </NavLink>
                     </button>
                   </div>
                 </div>
               </div>
-            </Link>
+            </NavLink>
           ))}
         </OwlCarousel>
       </section>
-      <footer className=" h-auto footer-div mt-5">
-        <div className="container">
-          <div className="row">
-            <div className="col-6">
-              <div className="app-div mt-5 rounded-3">
-                <h5 className="ms-3 pt-2 pb-1">Download Our App</h5>
-                <div className="app-content d-flex justify-content-center ">
-                  <div className="app-img me-2 ">
-                    <a href="a" className="w-100">
-                      <img
-                        className="w-100"
-                        src="https://cdn.media.amplience.net/i/xcite/Download_on_the_App_Store_Badge2?img404=default&w=384&qlt=75&fmt=auto"
-                        alt=""
-                      />
-                    </a>
-                  </div>
-                  <div className="app-img">
-                    <a href="a" className="w-100">
-                      <img
-                        className="w-100"
-                        src="https://cdn.media.amplience.net/i/xcite/Google_Play_Store_badge_EN2?img404=default&w=384&qlt=75&fmt=auto"
-                        alt=""
-                      />
-                    </a>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="line-footer mt-5"></div>
-        <div className="container mt-5">
-          <div className="row">
-            <div className="col-3">
-              <div className="footer-text1 text-white">
-                <h1 className="mb-3">SED</h1>
-                <p className=" fs-7">
-                  Xcite: The Ultimate Online Shopping Destination in Kuwait |
-                  Buy 100% genuine consumer electronics and home appliances from
-                  Xcite, the largest online electronics store in Kuwait.
-                </p>
-              </div>
-            </div>
-            <div className="col-3 mt-2">
-              <div className="footer-text2 text-white ">
-                <h5 className="mb-4">General Information</h5>
-                <a href="a" className=" d-block mb-3">
-                  Help
-                </a>
-                <a href="a" className=" d-block mb-3">
-                  About X-cite
-                </a>
-                <a href="a" className=" d-block mb-3">
-                  Privacy Policy
-                </a>
-                <a href="a" className=" d-block mb-3">
-                  Frequently Asked Questions
-                </a>
-                <a href="a" className=" d-block">
-                  Terms & Conditions
-                </a>
-              </div>
-            </div>
-            <div className="col-3 mt-2">
-              <div className="footer-text2 text-white ">
-                <h5 className="mb-4">Our Services</h5>
-                <a href="a" className=" d-block mb-3">
-                  Pay Monthly Installments
-                </a>
-                <a href="a" className=" d-block mb-3">
-                  Service Centers
-                </a>
-                <a href="a" className=" d-block mb-3">
-                  Contact Us
-                </a>
-                <a href="a" className=" d-block mb-3">
-                  Quick Delivery & Installation
-                </a>
-                <a href="a" className=" d-block mb-3">
-                  Buyer Protection
-                </a>
-                <a href="a" className=" d-block mb-3">
-                  X-cite Secure Shopping
-                </a>
-                <a href="a" className=" d-block mb-3">
-                  B2B - Corporate Sales
-                </a>
-              </div>
-            </div>
-            <div className="col-3 mt-2">
-              <div className="footer-text2 text-white ">
-                <h5 className="mb-4">Highlights</h5>
-                <a href="a" className=" d-block mb-3">
-                  flyer
-                </a>
-                <a href="a" className=" d-block mb-3">
-                  X-cite Mobile App
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="line-footer mt-5"></div>
-        <h5 className="text-white mt-4 pb-5">
-          © 2023 X-cite General Trading Co. S.A.K. (C)
-        </h5>
-      </footer>
+      <Footer />
     </>
   );
 }
