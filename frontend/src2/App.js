bd5287fd9a8819d14290fa77ad6462aab4dccbb4
Mohamed Adel <ahmrn1999@gmail.com>
2023-04-11 15:43:20 +0200

    login and profile

---
+++ frontend/src2/App.js
@@ -0,0 +1,26 @@
+
+import './App.css';
+import React from 'react';
+import Signin from './pages/Signin/Signin';
+
+function App() {
+  return (
+
+    <>
+    <Signin/>
+    
+    
+  
+    
+    
+    
+  
+    
+
+   
+
+    </>
+  )
+}
+
+export default App;
