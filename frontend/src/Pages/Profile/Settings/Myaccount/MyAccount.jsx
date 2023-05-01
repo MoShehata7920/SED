d32fa253cd134477b99595a447a2e29f648d479d
abdelrhman ezzat saad <101066128+abdoezzat1323@users.noreply.github.com>
2023-05-02 02:09:16 +0300

    acount setting

--- frontend/src/Pages/Profile/Settings/Myaccount/MyAccount.jsx
+++ frontend/src/Pages/Profile/Settings/Myaccount/MyAccount.jsx
@@ -3,91 +3,67 @@ import "./MyAccount.css";
 import "./user.png";
 import Userinfo from "../../Userinfo/Userinfo";
 
-
 function MyAccount() {
-  let [Editing ,setEditing] =useState(false)
-  const handelEditing =()=>{
-   setEditing(true)
-  }
+  let [Editing, setEditing] = useState(false);
+  const handelEditing = () => {
+    setEditing(true);
+  };
   return (
     <>
-     { Editing ?
-    <div className="container  ">
-      <div className="row vh-100  flex-column align-items-center justify-content-center   ">
-        <div className=" col-8 rounded-3 border border-dark  pt-3 pb-3">
-        
-          <div className="row ">
-            <div className=" offset-1 col-4 ">
-        
+      <div className="container  ">
+        <div className="row vh-100  flex-column align-items-center justify-content-center   ">
+          <div className=" col-8 rounded-3 border border-dark  pt-3 pb-3">
+            <div className="row ">
+              <div className=" offset-1 col-4 ">
                 <div class="mb-3">
-                    <label  class="form-label">user name </label>
-                    <input type="text" class="form-control"  aria-describedby="emailHelp"></input>
-                  </div>
+                  <label class="form-label">user name </label>
+                  <input
+                    type="text"
+                    class="form-control"
+                    aria-describedby="emailHelp"
+                  ></input>
+                </div>
 
-                  <div class="mb-3">
-                    <label class="form-label">Email </label>
-                    <input type="email" class="form-control" ></input>
-                  </div>
+                <div class="mb-3">
+                  <label class="form-label">Email </label>
+                  <input type="email" class="form-control"></input>
+                </div>
 
-                  <div class="mb-3">
-                    <label class="form-label">Password</label>
-                    <input type="password" class="form-control" ></input>
-                  </div>
-            </div>
-            <div className=" offset-2 col-4">
+                <div class="mb-3">
+                  <label class="form-label">Password</label>
+                  <input type="password" class="form-control"></input>
+                </div>
+              </div>
+              <div className=" offset-2 col-4">
+                <div class="mb-3">
+                  <label class="form-label">Location </label>
+                  <input type="text" class="form-control"></input>
+                </div>
 
-              
-                  <div class="mb-3">
-                      <label  class="form-label">Location </label>
-                      <input type="text" class="form-control" ></input>
-                    </div>
+                <div class="mb-3 "></div>
 
-                    <div class="mb-3 ">
-                    
-                      
-                    </div>
+                <div class="mb-3">
+                  <label class="form-label">date of birth </label>
+                  <input type="date" class="form-control"></input>
+                </div>
 
-                    <div class="mb-3">
-                      <label  class="form-label">date of birth </label>
-                      <input type="date" class="form-control" ></input>
-                    </div>
-
-                    <div class="mb-3">
-                      <label  class="form-label">mobile number  </label>
-                      <input type="text" class="form-control" ></input>
-                    </div>
-                    <div className="mb-3 d-flex justify-content-around ">
-                      <button type="button" class="btn btn-primary btn-sm ">Save Changes </button>
-                      <button type="button" class="btn btn-primary btn-sm ">Cancel </button>
-                    </div>
-
-                  </div>
-        
-          </div>
-
-          </div>
-        </div>
-      </div>
-
-    
-    :
-    <>
-    <section>
-          <div className="container">
-            <div className="row">
-              <div className="col">
-                <div onClick={handelEditing} className="btn">
-                   Edditing
-               </div>
+                <div class="mb-3">
+                  <label class="form-label">mobile number </label>
+                  <input type="text" class="form-control"></input>
+                </div>
+                <div className="mb-3 d-flex justify-content-around ">
+                  <button type="button" class="btn btn-primary btn-sm ">
+                    Save Changes{" "}
+                  </button>
+                  <button type="button" class="btn btn-primary btn-sm ">
+                    Cancel{" "}
+                  </button>
+                </div>
               </div>
             </div>
           </div>
-    </section>
-    <section>
-      <Userinfo/>
-    </section>
-    </>
-     }
+        </div>
+      </div>
     </>
   );
 }
--- frontend/src/Pages/Profile/Userinfo/Userinfo.jsx
+++ frontend/src/Pages/Profile/Userinfo/Userinfo.jsx
@@ -2,54 +2,51 @@ import "./Userinfo.css";
 import React from "react";
 import "./user.png";
 
-
 export default function Userinfo() {
-  return <>
- 
-<div className="container px-1">
-  <div className="row justify-content-center col-xs-12">
-    <div className="col-lg-8   border border-dark rounded bg-primary rounded-3 mt-5  ">
-      
-      <div className="row  ">
-       
+  return (
+    <>
+      <div className="container px-1">
+        <div className="row justify-content-center ">
+          <div className="col-lg-8   border border-dark rounded  rounded-3 mt-5  ">
+            <div className=" offset-10">
+              <div className="btn">Edditing</div>
+            </div>
+            <div className="d-flex py-3 ">
+              <div className="profile-user-info row py-3  px-1 justify-content-center ">
+                <div className="user-info row py-3">
+                  <h5 className="py-1 "> User Name </h5>
+                  <h6 className="w-75 "> mohamed adel </h6>
+                </div>
+                <div className="user-info row py-3">
+                  <h5 className="py-1 ">Email </h5>
+                  <h6 className="w-75 "> ahmen1999@gami.com</h6>
+                </div>
+
+                <div className=" user-info row py-3">
+                  <h5 className="py-1">Mobile Number </h5>
+                  <h6 className="w-75  ">01115572758</h6>
+                </div>
+              </div>
+              <div className="profile-user-info row py-3  px-1 justify-content-center ">
+                <div className=" user-info row py-3">
+                  <h5 className="py-1">Location </h5>
+                  <h6 className="w-100  ">tanta </h6>
+                </div>
+
+                <div className=" user-info row py-3">
+                  <h5 className="py-1">Gender</h5>
+                  <h6 className="w-100  ">male </h6>
+                </div>
+
+                <div className=" user-info  row py-3">
+                  <h5 className="py-1">Date Of Birth</h5>
+                  <h6 className="w-100  "> 17/6/1999</h6>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-     <div className="d-flex py-3 ">
-          <div className="profile-user-info row py-3  px-1 justify-content-center " >
-                    <div className="user-info row py-3">
-                      <h5 className="py-1 "> User Name  </h5>
-                      <h6 className="w-75 bg-primary"> mohamed adel </h6> 
-                    </div>
-                    <div className="user-info row py-3">
-                      <h5 className="py-1 ">Email </h5>
-                      <h6 className="w-75 bg-primary"> ahmen1999@gami.com</h6> 
-                    </div>
-
-                    <div className=" user-info row py-3">
-                      <h5 className="py-1">Mobile Number </h5>
-                      <h6 className="w-75 bg-primary ">01115572758</h6>   
-                    </div>
-          </div>
-          <div className="profile-user-info row py-3  px-1 justify-content-center " >
-                    <div className=" user-info row py-3">
-                      <h5 className="py-1">Location </h5>
-                      <h6 className="w-100  bg-primary">tanta </h6> 
-                    </div>
-
-                    <div className=" user-info row py-3">
-                        <h5 className="py-1">Gender</h5>
-                        <h6 className="w-100  bg-primary">male </h6>  
-                    </div>
-
-                    <div className=" user-info  row py-3">
-                        <h5 className="py-1">Date Of Birth</h5>
-                        <h6 className="w-100  bg-primary"> 17/6/1999</h6>     
-                    </div>
-          </div>
-     </div>
-    </div>
-  </div>
-</div>
-
-      
-   </>;
+    </>
+  );
 }
