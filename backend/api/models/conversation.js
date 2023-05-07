50d08250cf6a8f8ceb83bb117e00e7b4f8e10c9d
Mahmoud Elamrosy <74033364+melamrosy72@users.noreply.github.com>
2023-05-07 23:11:00 +0300

    chat adds

--- backend/api/models/conversation.js
+++ backend/api/models/conversation.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const conversationSchema = mongoose.Schema({
   users: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }],
-  messages: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Message' }],
+  // messages: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Message' }],
   createdAt: { type: Date, default: Date.now() },
 });
 
--- backend/api/routes/chat.js
+++ backend/api/routes/chat.js
@@ -3,10 +3,11 @@ const router = express.Router();
 const Conversation = require('../models/conversation');
 const Message = require('../models/messages');
 const User = require('../models/user');
+const mongoose=require('mongoose')
 
 
 //creating new message
-router.post("/", async (req, res) => {
+router.post("/new-message", async (req, res) => {
   const newMessage = new Message(req.body);
   try {
     const savedMessage = await newMessage.save();
@@ -17,13 +18,13 @@ router.post("/", async (req, res) => {
 });
 
 //getting conversation messages by conversation Id
-router.get("/:conversationId", async (req, res) => {
+router.get("/messages/:conversationId", async (req, res) => {
   try {
     const messages = await Message.find({
       conversation: req.params.conversationId,      //  conversation >> 'conversation id'
     });
     if(messages.length===0){
-      res.status(200).json({status:0 , message : "No messages yet",conversations})
+      res.status(200).json({status:0 , message : "No messages yet", messages })
     }
     res.status(200).json({status:0 , messages});
   } catch (err) {
@@ -35,14 +36,14 @@ router.get("/:conversationId", async (req, res) => {
 
 
 //getting user conversations by his Id
-router.get("/:userId", async (req, res) => {
+router.get("/user-convs/:userId", async (req, res) => {
   try {
     const conversations = await Conversation.find({
       // users: { $in: [req.params.userId] },
       users: { $in: [mongoose.Types.ObjectId(req.params.userId)] }, // for better security 'preventing injection attacks' 
     });
     if(conversations.length===0){
-      res.status(200).json({status:0 , message : "There are no conversations , start One !",conversations})
+      res.status(200).json({status:0 , message : "There are no conversations , start One !", conversations })
     }
     res.status(200).json({ status:0 , conversations });
   } catch (err) {
@@ -50,8 +51,21 @@ router.get("/:userId", async (req, res) => {
   }
 });
 
+//creating new conversation
+router.post("/new-conversation", async (req, res) => {
+  const newConversation = new Conversation({
+    users: [req.body.senderId, req.body.receiverId],
+  });
+  try {
+    const savedConversation = await newConversation.save();
+    res.status(200).json({status:0,savedConversation});
+  } catch (err) {
+    res.status(500).json({status:1 , message:err.message , err});
+  }
+});
 
-router.get('/:conversationId/:user/:receive', async (req, res) => {
+// getting conversation with its messages between two users by conv id and user id and reciever id
+router.get('/get/:conversationId/:user/:receive', async (req, res) => {
   const conversationId = req.params.conversationId;
   const currentUserId = req.params.user;
   const reciverId = req.params.receive;
@@ -59,51 +73,19 @@ router.get('/:conversationId/:user/:receive', async (req, res) => {
   try {
     const currentUser = await User.findById(currentUserId);
     const conversation = await Conversation.findById(conversationId).populate('users', '_id name')
-    if (!conversation) {
-      await new Conversation({
-        users: [currentUserId, reciverId]
-      }).save()
-    };
+    // if (!conversation) {
+    //   await new Conversation({
+    //     users: [currentUserId, reciverId]
+    //   }).save()
+    // };
     const messages = await Message.find({ conversation: conversationId }).populate('sender', 'fullName');
-    res.render('chatPage', { conversation, currentUser, messages });
+    // res.render('chatPage', { conversation, currentUser, messages });
+    res.status(200).json({status:0 ,currentUser, conversation ,  messages })
   } catch (err) {
     console.error(err);
-    res.status(500).json({ message: err });
+    res.status(500).json({status:1 , message:err.message , err });
   }
 });
 
-router.get('/:conversationId/:user/:receive', async (req, res) => {
-  const conversationId = req.params.conversationId;
-  const currentUserId = req.params.user;
-  const reciverId = req.params.receive;
-
-  try {
-    const currentUser = await User.findById(currentUserId);
-
-    // if messages are on conversation so why we don't get it directly
-    const conversation = await Conversation.findById(conversationId).populate({path: 'users',select: '_id name'})
-  .populate({
-    path: 'messages',
-    populate: {
-      path: 'sender',
-      model: 'User',
-      select: 'fullName'
-    },
-    select:'text'
-  });
-
-    if (!conversation) {
-      await new Conversation({
-        users: [currentUserId, reciverId]
-      }).save()
-    };
-    const messages=conversation.messages
-    // const messages = await Message.find({ conversation: conversationId }).populate('sender', 'fullName');
-    res.render('chatPage', { conversation, currentUser, messages });
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ message: err });
-  }
-});
 
 module.exports = router;
