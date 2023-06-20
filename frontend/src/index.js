8796611c6510450e4169f3d0a3fc5b8e1afaf2fe
abdelrhman ezzat saad <101066128+abdoezzat1323@users.noreply.github.com>
2023-06-20 19:01:15 +0300

    some eddit

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,11 +3,10 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import MainRoutes from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.bundle.js.map";
+import "bootstrap/dist/js/bootstrap.bundle.js";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import "owl.carousel/dist/assets/owl.carousel.css";
 import "owl.carousel/dist/assets/owl.theme.default.css";
-
 import "owl.carousel";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
