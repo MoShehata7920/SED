a0905227a2bc283887f0b6ac4dc3582b45bd8382
abdallah2799 <92616894+abdallah2799@users.noreply.github.com>
2023-07-06 19:45:55 +0300

    5

--- backend/api/routes/auth.js
+++ backend/api/routes/auth.js
@@ -2,9 +2,9 @@ const passportSetup = require('../config/passport-setup')
 const passport = require('passport')
 const router = require('express').Router()
 const authController = require('../controllers/auth');
-const {resetVerification , resetVerificationByOTP } = require('../middleware/check-reset')
+const { resetVerification, resetVerificationByOTP } = require('../middleware/check-reset')
 const { body, validationResult } = require('express-validator');
-const { verifyTokenAndAdmin, verifyToken , verifyTokenAndAuthorization } = require('../middleware/check-auth')
+const { verifyTokenAndAdmin, verifyToken, verifyTokenAndAuthorization } = require('../middleware/check-auth')
 
 router.post('/register',
     body('email').not().isEmpty().withMessage('Empty Mail Field').isEmail().withMessage('Not A Valid Mail'),
@@ -18,9 +18,9 @@ router.post('/register',
 
 
 // verify email
-router.post('/verifyemail',verifyToken,
-body('code').not().isEmpty().withMessage('Empty code Field'),
-authController.verifyEmailByOtp);
+router.post('/verifyemail', verifyToken,
+    body('code').not().isEmpty().withMessage('Empty code Field'),
+    authController.verifyEmailByOtp);
 
 router.post('/login',
     body('loginOption').not().isEmpty().withMessage('Empty Mail Or Phone Field'),
@@ -41,7 +41,7 @@ router.get('/google/redirect', passport.authenticate('google', { failureRedirect
 
 router.post('/forgot', authController.forgotPassword);
 
-router.post('/reset',resetVerification,
+router.post('/reset', resetVerification,
     body('password').not().isEmpty().withMessage('Empty Password Field'),
     body('password').isLength({ min: 5 }).matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/).withMessage('weak password'),
     body('confirmPassword').custom((value, { req }) => {
@@ -56,23 +56,23 @@ router.get('/logout', (req, res) => {
 });
 
 // @route for resending email verification
-router.post('/resendVerifyEmail',verifyToken,authController.resendVerifyEmail)
+router.post('/resendVerifyEmail', verifyToken, authController.resendVerifyEmail)
 
 
 //new edits for mobile app to send otp as the email verify with  otp
 router.post('/forgotOTP', authController.forgotPasswordByOTP);
 router.post('/resetOTP',
     body('code').matches(/^.{6}$/)  //  validating is onyl 6 chars
-    .withMessage('Code must be 6 characters long.')
+        .withMessage('Code must be 6 characters long.')
     , authController.VerifyresetPasswordOTP);
-router.patch('/verified-pw-change',verifyTokenAndAuthorization
-    ,[
-    body('password').isLength({ min: 5 }).matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/).withMessage('weak password , Enter a strong one that contains cap chars and numbers'),
-    body('confirmPassword').custom((value, { req }) => {
-        if (value !== req.body.password) { throw new Error('Password And Confirmation Doesn\'t Match'); }
-        return true;
-    }),
-    ],authController.verifiedPwChange)
+router.patch('/verified-pw-change'
+    , [
+        body('password').isLength({ min: 5 }).matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/).withMessage('weak password , Enter a strong one that contains cap chars and numbers'),
+        body('confirmPassword').custom((value, { req }) => {
+            if (value !== req.body.password) { throw new Error('Password And Confirmation Doesn\'t Match'); }
+            return true;
+        }),
+    ], authController.verifiedPwChange)
 //////////////////
 
 module.exports = router;
