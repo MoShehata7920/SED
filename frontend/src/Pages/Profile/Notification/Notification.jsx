2766cac07b74a85b819d99ab097fe79f78dc3316
abdelrhman ezzat saad <101066128+abdoezzat1323@users.noreply.github.com>
2023-07-16 04:38:29 +0300

    last

--- frontend/src/Pages/Profile/Notification/Notification.jsx
+++ frontend/src/Pages/Profile/Notification/Notification.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import "./Notification.css";
 import Userinfo from "../Userinfo/Userinfo";
+import Toast from "react-bootstrap/Toast";
+import ToastContainer from "react-bootstrap/ToastContainer";
 
 function Notification() {
   return (
@@ -12,7 +14,66 @@ function Notification() {
             <h1>Notification</h1>
           </div>
           <div className="  bg-black pading_05"></div>
-          <div className=""></div>
+          <div className=" w-50  mt-4">
+            <ToastContainer className="position-static w-100">
+              <Toast className="w-100">
+                <Toast.Header>
+                  <img
+                    src="holder.js/20x20?text=%20"
+                    className="rounded me-2"
+                    alt=""
+                  />
+                  <strong className="me-auto">Admin</strong>
+                  <small className="text-muted">just now</small>
+                </Toast.Header>
+                <Toast.Body>
+                  you need to change youre password evry 30 days
+                </Toast.Body>
+              </Toast>
+              <Toast className="w-100">
+                <Toast.Header>
+                  <img
+                    src="holder.js/20x20?text=%20"
+                    className="rounded me-2"
+                    alt=""
+                  />
+                  <strong className="me-auto">Admin</strong>
+                  <small className="text-muted">just now</small>
+                </Toast.Header>
+                <Toast.Body>
+                  you need to change youre password evry 30 days
+                </Toast.Body>
+              </Toast>
+              <Toast className="w-100">
+                <Toast.Header>
+                  <img
+                    src="holder.js/20x20?text=%20"
+                    className="rounded me-2"
+                    alt=""
+                  />
+                  <strong className="me-auto">Admin</strong>
+                  <small className="text-muted">just now</small>
+                </Toast.Header>
+                <Toast.Body>
+                  you need to change youre password evry 30 days
+                </Toast.Body>
+              </Toast>
+              <Toast className="w-100">
+                <Toast.Header>
+                  <img
+                    src="holder.js/20x20?text=%20"
+                    className="rounded me-2"
+                    alt=""
+                  />
+                  <strong className="me-auto">Admin</strong>
+                  <small className="text-muted">just now</small>
+                </Toast.Header>
+                <Toast.Body>
+                  you need to change youre password evry 30 days
+                </Toast.Body>
+              </Toast>
+            </ToastContainer>
+          </div>
         </div>
       </section>
     </>
